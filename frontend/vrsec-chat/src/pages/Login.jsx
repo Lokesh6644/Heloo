f83c4015c4133f24fdf2622cfd464c7844e90b8f
Lokesh <238w1a12a3@vrsec.ac.in>
2026-02-25 21:49:42 +0530

    Updated UI styling

--- frontend/vrsec-chat/src/pages/Login.jsx
+++ frontend/vrsec-chat/src/pages/Login.jsx
@@ -1,37 +1,36 @@
 import { GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
-import { useNavigate } from "react-router-dom";
+//import { jwtDecode } from "jwt-decode";
+//import { useNavigate } from "react-router-dom";
 
 export default function Login() {
 
-  const navigate = useNavigate();
+ // const navigate = useNavigate();
 
   return (
     <div className="auth-container">
+      <h1 className="bg-heading">
+    Turning Hello's into Friendships...
+  </h1>
       <div className="auth-card">
-        <h2>Sign in with College Email</h2>
 
-        <GoogleLogin
-          onSuccess={credentialResponse => {
+   
 
-            const decoded = jwtDecode(credentialResponse.credential);
+<p className="auth-ticker">
+  <span>
+    🚨 Please sign in using your official college email (@vrsec.ac.in). 
+    Other email domains will be restricted from accessing VRSEC Campus Chat.
+  </span>
+</p>
 
-            const email = decoded.email;
+        <h2> Oye</h2>
 
-            if (!email.endsWith("@vrsec.ac.in")) {
-              alert("Only college emails allowed");
-              return;
-            }
-
-            localStorage.setItem("googleToken", credentialResponse.credential);
-
-            navigate("/chat");
-          }}
-          onError={() => {
-            console.log("Login Failed");
-          }}
-        />
-
+         <GoogleLogin
+    theme="filled_blue"
+    size="large"
+    shape="pill"
+    width="420"
+  />
+      
       </div>
     </div>
   );
